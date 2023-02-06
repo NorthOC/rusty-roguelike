120a42fb8e6082f43aea2b500280187db009bf5c
NorthOC <rev.denisas@gmail.com>
2023-02-06 15:37:12 +0200

    initial

---
+++ src/main.rs
@@ -0,0 +1,68 @@
+mod map;
+mod map_builder;
+mod player;
+mod prelude {
+    pub use bracket_lib::prelude::*;
+    pub const SCREEN_WIDTH: i32 = 80;
+    pub const SCREEN_HEIGHT: i32 = 50;
+    pub const DISPLAY_WIDTH: i32 = SCREEN_WIDTH / 2;
+    pub const DISPLAY_HEIGHT: i32 = SCREEN_HEIGHT / 2;
+    pub use crate::map::*;
+    pub use crate::map_builder::*;
+    pub use crate::player::*;
+}
+use prelude::*;
+//1. Create a basic dungeon map *DONE*
+//2. Place the player and let them walk around *DONE*
+//3. Spawn monsters, draw them, and let the player kill them by walking into them.
+//4. Add health and a combat system that uses it.
+//5. Add healing potions.
+//6. Display a “game over” screen when the player dies.
+//7. Add the Amulet of Yala to the level and let the player win by reaching it.
+
+struct State {
+    map: Map,
+    player: Player,
+}
+
+impl State {
+    fn new() -> Self {
+        let mut rng = RandomNumberGenerator::new();
+        let map_builder = MapBuilder::new(&mut rng);
+        State {
+            map: map_builder.map,
+            player: Player::new(map_builder.player_start),
+        }
+    }
+}
+
+impl Default for State {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl GameState for State {
+    fn tick(&mut self, ctx: &mut BTerm) {
+        ctx.cls();
+        self.player.update(ctx, &self.map);
+        self.map.render(ctx);
+        self.player.render(ctx);
+    }
+}
+
+fn main() -> BError {
+    println!("Hello, world!");
+    let context = BTermBuilder::new()
+        .with_title("Dungeon Crawler")
+        .with_fps_cap(30.0)
+        .with_dimensions(DISPLAY_WIDTH, DISPLAY_HEIGHT)
+        .with_tile_dimensions(32, 32)
+        .with_resource_path("resources/")
+        .with_font("dungeonfont.png", 32, 32)
+        .with_simple_console(DISPLAY_WIDTH, DISPLAY_HEIGHT, "dungeonfont.png")
+        .with_simple_console_no_bg(DISPLAY_WIDTH, DISPLAY_HEIGHT, "dungeonfont.png")
+        .build()?;
+
+    main_loop(context, State::new())
+}
---
+++ src/map.rs
@@ -0,0 +1,60 @@
+use crate::prelude::*;
+const NUM_TILES: usize = (SCREEN_WIDTH * SCREEN_HEIGHT) as usize;
+
+//tiles
+#[derive(Clone, Copy, PartialEq)]
+pub enum TileType {
+    Wall,
+    Floor,
+}
+
+//map
+pub struct Map {
+    pub tiles: Vec<TileType>,
+}
+
+impl Map {
+    pub fn new() -> Self {
+        Map {
+            tiles: vec![TileType::Floor; NUM_TILES],
+        }
+    }
+    // render the map row-first striding
+    pub fn render(&self, ctx: &mut BTerm) {
+        for y in 0..SCREEN_HEIGHT {
+            for x in 0..SCREEN_WIDTH {
+                let idx = map_idx(x, y);
+                match self.tiles[idx] {
+                    TileType::Floor => {
+                        ctx.set(x, y, YELLOW, BLACK, to_cp437(' '));
+                    }
+                    TileType::Wall => {
+                        ctx.set(x, y, DARKSLATEGRAY4, BLACK, to_cp437('#'));
+                    }
+                }
+            }
+        }
+    }
+    // check if entity is in bounds of screen
+    pub fn in_bounds(&self, point: Point) -> bool {
+        point.x < SCREEN_WIDTH && point.x >= 0 && point.y < SCREEN_HEIGHT && point.y >= 0
+    }
+    // check if entity can walk on tile
+    pub fn can_enter_tile(&self, point: Point) -> bool {
+        let tile = map_idx(point.x, point.y);
+        self.in_bounds(point) && self.tiles[tile] == TileType::Floor
+    }
+    // check if requested tile is out of bounds or not
+    pub fn try_idx(&self, point: Point) -> Option<usize> {
+        if !self.in_bounds(point) {
+            None
+        } else {
+            Some(map_idx(point.x, point.y))
+        }
+    }
+}
+
+// calculate tile index in a 1D vec
+pub fn map_idx(x: i32, y: i32) -> usize {
+    ((y * SCREEN_WIDTH) + x) as usize
+}
